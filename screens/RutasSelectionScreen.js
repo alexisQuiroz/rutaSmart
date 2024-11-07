f32ecc3cfd0327c4f34bb5befdaa08cb20d1f9e7
Alexis Quiroz <alexis.quiroz@senda.gob.cl>
2024-11-06 22:41:07 -0300

    functiona ejemplo de mapa en un trayecto

--- screens/RutasSelectionScreen.js
+++ screens/RutasSelectionScreen.js
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import MapViewDirections from 'react-native-maps-directions';
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyB8c8sGA9H8s1RtwffWi4vKSSluR4ZZ8eQ';
 
 const RutasSelectionScreen = () => {
-  const [region, setRegion] = useState(null);
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState({
+    latitude: -33.447055540026135,  // San Francisco latitude
+    longitude: -70.64935529499316// San Francisco longitude
+    
+  });
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
@@ -18,24 +26,40 @@ const RutasSelectionScreen = () => {
 
       // Obtener la ubicación actual
       let location = await Location.getCurrentPositionAsync({});
-      setRegion({
+      setOrigin({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
       });
     })();
   }, []);
 
   return (
     <View style={styles.container}>
-      {region ? (
-        <MapView style={styles.map} region={region}>
-          <Marker
-            coordinate={{ latitude: region.latitude, longitude: region.longitude }}
-            title="Tu ubicación"
-            description="Aquí estás"
-          />
+      {origin ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: origin.latitude,
+            longitude: origin.longitude,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+        >
+          <Marker coordinate={origin} title="Origen" description="Ubicación actual" />
+          <Marker coordinate={destination} title="Destino" description="Destino" />
+
+          {/* Componente para mostrar la ruta */}
+          <MapViewDirections
+  origin={origin}
+  destination={destination}
+  apikey={GOOGLE_MAPS_APIKEY}
+  strokeWidth={4}
+  strokeColor="blue"
+  onError={(error) => {
+    console.log("Error al obtener la dirección:", error);
+    alert('Error al obtener la dirección: ' + error.message);
+  }}
+/>
         </MapView>
       ) : (
         <View style={styles.loading}>
